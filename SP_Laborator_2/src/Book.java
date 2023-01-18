9f4642bdc23e58558600000291d58522a29b95a7
Ionut-T-UVT <75988904+Ionut-T-UVT@users.noreply.github.com>
2023-01-18 21:23:39 +0200

    Add files via upload

---
+++ SP_Laborator_2/src/Book.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private String title;
+    private List<Chapter> chapters;
+
+    private List<Author> authors;
+
+    public Book (String title){
+        this.title = title;
+        chapters = new ArrayList<>();
+        authors = new ArrayList<>();
+    }
+
+    public void print(){
+        System.out.println("Chestie book:");
+        for(Chapter c: chapters)
+           c.print();
+        for(Author a: authors)
+            a.print();
+    }
+
+    public int createChapter(String title){
+        Chapter chapter = new Chapter(title);
+        chapters.add(chapter);
+        return chapters.size()-1;
+    }
+
+    public Chapter getChapter(int index){
+        return chapters.get(index);
+    }
+
+    public void addAuthor(Author a){
+        authors.add(a);
+    }
+}
---
+++ SP_Laborator_2/src/SubChapter.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubChapter {
+    private String name;
+    private List<Element> elements;
+
+    public SubChapter(String name){
+        this.name = name;
+        elements = new ArrayList<>();
+    }
+
+   public void createNewParagraph( String a){
+        Paragraph paragraph = new Paragraph(a);
+        elements.add(paragraph);
+   }
+
+    public void createNewImage( String a){
+        Image image = new Image(a);
+        elements.add(image);
+    }
+
+    public void createNewTable( String a){
+        Table table = new Table(a);
+        elements.add(table);
+    }
+
+    public void print(){
+        System.out.println("Chestii Subchapter:");
+                for(Element e: elements)
+                    e.print();
+    }
+}
